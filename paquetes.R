6c7d3699eed8f52c2656f0fc7fcd41830fd8245f
psotob <percys1991@gmail.com>
2022-09-07 16:15:26 -0500

    Update

---
+++ paquetes.R
@@ -0,0 +1,14 @@
+pacman::p_load(DescTools,
+               ggpubr,
+               ggsurvfit,
+               naniar,
+               plotly,
+               psych,
+               summarytools,
+               survminer,
+               treemap,
+               VIM,
+               visdat,
+               webshot2)
+
+pacman::p_load(gtsummary, flextable, huxtable, survival, survminer, ggsurvfit)
